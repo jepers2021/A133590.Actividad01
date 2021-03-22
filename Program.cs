00278e508d7472eede6bf8195b5e627fa4d4e47d
jepers2021 <jepers2021@gmail.com>
2021-03-22 13:48:23 -0300

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A133590.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola mundo!");
+            Console.ReadKey();
+        }
+    }
+}
